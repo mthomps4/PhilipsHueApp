a716a12ea9c813b93900481125290b00cba7dfa3
Matt Thompson <matthew.thompson.a@gmail.com>
2017-04-30 18:21:35 -0400

    Test Push

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <div className="App">
           <div className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h2>3Wire Smart Light System</h2>
+            {/* <h2>3Wire Smart Light System</h2> */}
           </div>
           <LinkBridge />
         </div>
